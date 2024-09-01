8e2141abf1ec423b593467530952f5283bbbb5dc
Kasiandra <65874443+Kaisa-Marysia@users.noreply.github.com>
2024-09-01 13:21:39 +0200

    Update kk-get-orders.sh

--- script/kk-get-orders.sh
+++ script/kk-get-orders.sh
@@ -7,13 +7,19 @@ TIMESTAMP=$(date +%Y_%m_%d-%H-%M-%S)
 REMOTEUSER=""
 DOMAIN=""
 DBUSER=""
+SMTP=""
+MFROM=""
+MTO=""
+MUSER=""
+MPASSWORD=""
+MFQDN=""
 
 if [[ $(ssh ${REMOTEUSER}@${DOMAIN} -i /home/${USER}/.ssh/kaoskvlt.de "mysql --silent --binary-as-hex  -e 'SELECT EXISTS(SELECT 1 FROM \`order\`);' -u ${DBUSER} -p"$PASSWORD" db-1") == 1 ]];
   then
     echo $TIMESTAMP": New Orders"
     ssh -i /home/${USER}/.ssh/ssh.key ${REMOTEUSER}@${DOMAIN} "mysql --binary-as-hex -udb-user-1 -p"$PASSWORD" db-1" < $WHEREAMI/get-order-querie.sql > /home/${USER}/kaoskvlt.de/order/$TIMESTAMP.txt
     ssh -i /home/${USER}/.ssh/ssh.key ${REMOTEUSER}@${DOMAIN} 'php bin/console cache:clear'
-    /bin/bash $WHEREAMI/mail.sh
+    sendEmail -u KaosKvltOrder -s ${SMTP} -f ${MFROM} -t ${MTO}  -xu ${MUSER} -xp ${MPASSWORD} -o fqdn=${MFQDN} -o tls=auto -m "New Order Was Sent. Check local order files"
     cd /home/${USER}/kaoskvlt.de/order
     awk '/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/{filename=NR"'$TIMESTAMP'.txt"}; {print >filename}' $TIMESTAMP.txt
     mv $TIMESTAMP.txt ../done-orders/
